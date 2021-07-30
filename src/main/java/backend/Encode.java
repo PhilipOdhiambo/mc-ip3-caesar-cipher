18ee347375d17bf991cb9357573b18540834e529
Philip Odhiambo <jofonowino@gmail.com>
2021-07-30 18:01:32 +0300

    test to encode 'a' to 'b' fails

--- src/main/java/backend/Encode.java
+++ src/main/java/backend/Encode.java
@@ -1,4 +1,9 @@
 package backend;
 
+import java.util.ArrayList;
+
 public class Encode {
+    public ArrayList<Object> encode(char input) {
+        return null;
+    }
 }
--- src/test/java/backend/EncodeTest.java
+++ src/test/java/backend/EncodeTest.java
@@ -1,13 +1,21 @@
 package backend;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncodeTest {
     @Test
-    public void decodeString_acceptNonNumberString_String() {
-
+    public void encode_lowerCase_char() {
+        // Encode a lower case letter
+        Encode testEncode = new Encode();
+        List<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add('b');
+        Assertions.assertEquals(expectedOutput, testEncode.encode('a'));
     }
 
 }

81880a627bb643b54cc47baa3c05c2f5956ce842
Philip Odhiambo <jofonowino@gmail.com>
2021-07-30 16:23:51 +0300

    intitialize test

--- src/test/java/backend/EncodeTest.java
+++ src/test/java/backend/EncodeTest.java
@@ -1,8 +1,13 @@
 package backend;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncodeTest {
+    @Test
+    public void decodeString_acceptNonNumberString_String() {
 
+    }
 
 }

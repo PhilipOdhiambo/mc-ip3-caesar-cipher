c869e24249341dbec563f00dd67e9d4a5a839a30
Philip Odhiambo <jofonowino@gmail.com>
2021-07-31 22:20:56 +0300

    test to translate non alphabet and previous tests pass

--- src/main/java/backend/Decode.java
+++ src/main/java/backend/Decode.java
@@ -2,7 +2,16 @@ package backend;
 
 public class Decode {
     public char decode(char valueToDecode, int decodeKey) {
-        int output = (int) valueToDecode - decodeKey;
-        return (char) output;
+
+        if (valueToDecode > 'A' && valueToDecode < 'Z') {
+            // Decode capital letter
+            return (char) ((int) valueToDecode - decodeKey);
+
+        } else if (valueToDecode > 'a' && valueToDecode < 'z') {
+            // Decode lower case letter
+            return (char) ((int) valueToDecode - decodeKey);
+        }
+
+        return (char) valueToDecode;
     }
 }
